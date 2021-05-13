f910cb81969569302f61c03928cf2b55c618a183
charleshlin <charles.lin@nbcuni.com>
2021-05-13 11:28:23 -0400

    Create bad.js

---
+++ bad.js
@@ -0,0 +1,9 @@
+#all fake
+var SSN = "154391243";
+var SSN2 = "532-52-4513";
+var social_security_number = "283942352";
+var social_security_number2 = '235-13-5235';
+var serialnumber = "SDF2-3423-DFDS-5454"
+var aws_access_key_id = "AKIAIOSFODNN7EXAMPLI";
+var aws_secret_access_key = "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";
+var pat = "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";
